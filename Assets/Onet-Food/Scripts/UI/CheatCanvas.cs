92f58e3c11cdf11c68234de9281d8f1e48e9475c
son24tuoi <son23tuoi@gmail.com>
2026-05-16 19:53:23 +0700

    + Add cheat

--- Assets/Onet-Food/Scripts/UI/CheatCanvas.cs
+++ Assets/Onet-Food/Scripts/UI/CheatCanvas.cs
@@ -1,11 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheatCanvas : MonoBehaviour
 {
-    public void OnClickGPGSButton()
+    public TMP_Dropdown levelDropdown;
+
+    public LevelManagerProfileSO levelManagerProfileSO;
+
+    private void Start()
     {
-        
+        levelDropdown.options.Clear();
+
+        for (int i = 0; i <= levelManagerProfileSO.MaxLevelIndex; i++)
+        {
+            levelDropdown.options.Add(new TMP_Dropdown.OptionData($"Level {i + 1}"));
+        }
+
+        levelDropdown.value = DataManager.Instance.Data.levelData.LevelIndex;
+
+        levelDropdown.onValueChanged.RemoveAllListeners();
+        levelDropdown.onValueChanged.AddListener(OnValueChanged_Level);
+    }
+
+    public void OnValueChanged_Level(int index)
+    {
+        DataManager.Instance.Data.levelData.LevelIndex = index;
+    }
+
+    public void OnClick_Add100Star()
+    {
+        DataManager.Instance.AddItem(ItemsData.ItemType.Star, 100);
+    }
+
+    public void OnClick_Add100Coin()
+    {
+        DataManager.Instance.AddItem(ItemsData.ItemType.Coin, 100);
     }
 }
